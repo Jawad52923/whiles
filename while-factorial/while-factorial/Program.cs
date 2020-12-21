1b7c1b51b40c7f249b8a9e0f3c861f9144e447b9
Your Name <you@example.com>
2020-12-21 20:55:53 +0500

    first commit

---
+++ while-factorial/while-factorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace while_factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your factorial number = ");
+            int n = Convert.ToInt16(Console.ReadLine());
+            int result = 1;
+
+            while (true)
+            {
+                Console.Write(n);
+                if (n == 1)
+                {
+                    break;
+                }
+                Console.Write(" * ");
+                result *= n;
+                n--;
+            }
+            Console.WriteLine(" = {0}", result);
+        }
+    }
+}
---
+++ while-for loop/while-for loop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace while_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 5)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i + " ");
+                }
+
+                Console.WriteLine();
+                i++;
+            }
+        }
+    }
+}
